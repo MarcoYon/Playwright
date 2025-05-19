e7cac441bde3ac948eda128666fb0fb4c3d8811c
Informes Marco <informesmarco@gmail.com>
2025-05-18 20:10:25 -0500

    Adecuando el proyecto al patron page object model

---
+++ stepDefinitions/loginStepDefinition.js
@@ -0,0 +1,15 @@
+const { Given,When, Then } = require('@cucumber/cucumber')
+
+Given('que me logueo con mi usuario {string} y contrasena {string}', function (userEmail , password) {
+    return 'pending';
+});
+
+When('agregue el item {string} al carrito de comrpas', function (item) {
+    return 'pending';
+});
+
+Then('validar que el item {string} este agregado en el carro de comrpas', function (item) {
+           // Write code here that turns the phrase above into concrete actions
+     return 'pending';
+});
+         

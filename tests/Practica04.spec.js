934b517a28cbc1a5d57c24ad638ccd5e6acdcb81
Marco Yon <marcoyon01.1991@gmail.com>
2025-05-27 19:33:19 -0500

    Detalle de orden de compra

--- tests/Practica04.spec.js
+++ tests/Practica04.spec.js
@@ -71,13 +71,41 @@ test.only('Client App Login', async ({page})=>
         if(text.trim() === "India") 
         {
             await dropdown.locator("button").nth(i).click();
+            break;
         }
 
     }
 
+    expect (page.locator(".user__name [type='text']").first()).toHaveText(email);
+    await page.locator(".action__submit").click();
+    await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ")
+    const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+    console.log("Numero de orden: " + orderId);
+
     //----------------- Agregando al  Carrito de Compras - END
 
+    //----------------- Detalle orden de compra
 
+    await page.locator("button[routerlink*='myorders']").click();
+
+    await page.locator("tbody").waitFor();
+    const rows = await page.locator("tbody tr");
+
+    for(let i =0; i < await rows.count(); ++i)
+    {
+        const rowOrderId= await rows.nth(i).locator("th").textContent();
+        if(orderId.includes(rowOrderId))
+        {
+            await rows.nth(i).locator("button").first().click();
+            break;
+        }
+
+    }
+
+    const orderIdDetails = await page.locator(".col-text").textContent();
+    expect(orderId.includes(orderIdDetails)).toBeTruthy();
+
+    //----------------- Detalle orden de compra - END
 
 
     await page.pause();
